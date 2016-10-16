947038990a10fbed76b3da35ab41091e1d8060f7
Charles Wang <happy.charles.wang@gmail.com>
2016-10-16 03:46:45 +0000

    add navigator

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -4,7 +4,7 @@
             <h1><a href="{{ url('/') }}">{{ Settings::blogTitle() }}</a></h1>
             <h3>{{ Settings::blogSubTitle() }}</h3>
             @if(isset($user->twitter) && strlen($user->twitter))
-                <a href="http://twitter.com/{{ $user->twitter }}" target="_blank" id="social"><i class="fa fa-fw fa-twitter"></i></a>
+                <a href="https://sharethemeal.org" target="_blank" id="social"><i class="fa fa-fw fa-heart"></i></a>
             @endif
             @if(isset($user->facebook) && strlen($user->facebook))
                 <a href="http://facebook.com/{{ $user->facebook }}" target="_blank" id="social"><i class="fa fa-fw fa-facebook"></i></a>
@@ -17,4 +17,15 @@
             @endif
         </div>
     </div>
+
+    <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+        <ol class="breadcrumb" style="background-color:white">
+            <li><a href="/">Home</a></li>
+            <li><a href="/blog?tag=Research">Research</a></li>
+            <li><a href="/blog/publications">Publications</a></li>
+            <li><a href="/blog/about">About</a></li>
+        </ol>
+    </div>
 </div>
+
+
